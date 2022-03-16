8f0e7ce965c90ef2aabf6b869a4e948e714898f8
Kirill <14krabik14@gmail.com>
2022-03-16 12:29:46 +0300

    correction of mistakes

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'a4b638bf722c32af8119890458a81911ec4573ff',
+    'name' => '14kra/oxem',
+  ),
+  'versions' => 
+  array (
+    '14kra/oxem' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'a4b638bf722c32af8119890458a81911ec4573ff',
+    ),
+  ),
+);
